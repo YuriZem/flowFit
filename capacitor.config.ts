11bb708b2fc064a223f5d60bdf77131ef92e00db
YuriMenoo <yuri.dev.menoo@gmail.com>
2025-02-08 04:10:25 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'flowFit',
+  webDir: 'www'
+};
+
+export default config;
